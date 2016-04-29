16366edc8411368bb119f2f1ebecadf09b20bb01
Cosmin Cioacla <tehciolo@gmail.com>
2016-04-29 23:13:43 +0300

    positioning

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,5 +1,544 @@
 module.exports = function() {
   return {
-    
+    '06': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '8%'
+            },
+            zone: {
+              top: '8%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '07': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '8%'
+            },
+            zone: {
+              top: '8%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '09': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '9%'
+            },
+            zone: {
+              top: '9%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '10': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '8%'
+            },
+            zone: {
+              top: '8%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '13': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '5%'
+            },
+            zone: {
+              top: '5%',
+              bottom: '29%'
+            }
+          }
+        }
+      ]
+    },
+    '14': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '3%'
+            },
+            zone: {
+              top: '3%',
+              bottom: '27%'
+            }
+          }
+        }
+      ]
+    },
+    '16': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '5%'
+            },
+            zone: {
+              top: '5%',
+              bottom: '45%'
+            }
+          }
+        }
+      ]
+    },
+    '18': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '4%'
+            },
+            zone: {
+              top: '4%',
+              bottom: '2%'
+            }
+          }
+        }
+      ]
+    },
+    '22': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '8%'
+            },
+            zone: {
+              top: '8%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '26': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '6%'
+            },
+            zone: {
+              top: '6%',
+              bottom: '4%'
+            }
+          }
+        }
+      ]
+    },
+    '27': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '6%'
+            },
+            zone: {
+              top: '6%',
+              bottom: '4%'
+            }
+          }
+        }
+      ]
+    },
+    '29': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '5%'
+            },
+            zone: {
+              top: '5%',
+              bottom: '30%'
+            }
+          }
+        }
+      ]
+    },
+    '32': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '6%'
+            },
+            zone: {
+              top: '6%',
+              bottom: '35%'
+            }
+          }
+        }
+      ]
+    },
+    '34': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '5%'
+            },
+            zone: {
+              top: '5%',
+              bottom: '22%'
+            }
+          }
+        }
+      ]
+    },
+    '46': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '6%'
+            },
+            zone: {
+              top: '6%',
+              bottom: '45%'
+            }
+          }
+        }
+      ]
+    },
+    '48': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '6%'
+            },
+            zone: {
+              top: '6%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '49': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '6%'
+            },
+            zone: {
+              top: '6%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '50': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '6%'
+            },
+            zone: {
+              top: '6%',
+              bottom: '28%'
+            }
+          }
+        }
+      ]
+    },
+    '51': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '42%'
+            },
+            zone: {
+              top: '42%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '52': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '47%'
+            },
+            zone: {
+              top: '47%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '53': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '47%'
+            },
+            zone: {
+              top: '47%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '54': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '7%'
+            },
+            zone: {
+              top: '7%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '57': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '7%'
+            },
+            zone: {
+              top: '7%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '58': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '5%'
+            },
+            zone: {
+              top: '5%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '62': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '7%'
+            },
+            zone: {
+              top: '7%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '64': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '8%'
+            },
+            zone: {
+              top: '8%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '67': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '58%'
+            },
+            zone: {
+              top: '58%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '70': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '14%'
+            },
+            zone: {
+              top: '14%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '76': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '7%'
+            },
+            zone: {
+              top: '7%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    },
+    '77': {
+      video: [
+        {
+          video: {
+            source: 'test'
+          },
+          trigger: {
+            button: {
+              top: '7%'
+            },
+            zone: {
+              top: '7%',
+              bottom: '3%'
+            }
+          }
+        }
+      ]
+    }
   }
 }
